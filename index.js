6bc49ea9b63c920f8ae2a90082c84212ca5f48e4
Cangyu Lin <jefflin.je598@gmail.com>
2022-10-12 16:36:17 +0800

    add storage system

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const multer = require('multer')
 const app = express()
 const port = 3000
-
+const storage =   multer.diskStorage({  
+  destination: function (req, file, callback) {  
+    callback(null, './src/imgs/');  
+  },  
+  filename: function (req, file, callback) {  
+    callback(null, file.originalname);  
+  }  
+});  
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
