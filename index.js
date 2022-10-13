3238123b86036e47dfd34079c7f2fc7a91690fb2
Cangyu Lin <jefflin.je598@gmail.com>
2022-10-13 10:59:00 +0800

    update the newlines

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const multer = require('multer')
-const upload = multer({dest: 'uploads/'})
+const upload = multer({dest: 'src/imgs/'})
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!<br>This is the home page of the website')
 })
+
+app.post('/upload', upload.single('uploaded_file'), function (req, res) {
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
