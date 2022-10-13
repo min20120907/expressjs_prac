d4fb6f0fbfecb98a76c67061dd2d026eb5d8da1b
Cangyu Lin <jefflin.je598@gmail.com>
2022-10-13 11:31:35 +0800

    add upload messages

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ const upload = multer({dest: 'src/imgs/'})
 const app = express()
 const port = 3000
 
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!<br>This is the home page of the website')
 })
 
+// upload messages
 app.post('/upload', upload.single('uploaded_file'), function (req, res) {
+	upload(req, res, function(err) {
+		if(err){
+			return res.end("Error uploading file.")
+		}
+	})
 
-});
+	res.end("The file is uploaded successfully!")
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
